71bc7411ecd6c5c39e51b6859e32baeb5bffa445
Ulaş Sancak <ulas.sancak@hotmail.com.tr>
2013-07-25 14:37:42 +0300

    Updated
    Updated

---
+++ ComboBoxExample/ViewController.m
@@ -0,0 +1,42 @@
+//
+//  ViewController.m
+//  ComboBoxExample
+//
+//  Created by Ulaş Sancak on 7/25/13.
+//  Copyright (c) 2013 Sancak. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    ComboBox *comboBox = [[ComboBox alloc]initWithFrame:CGRectMake(50, 50, 220, 36)];
+    comboBox.delegate = self;
+    [comboBox setComboBoxSize:CGSizeMake(220, 44*4)];
+  //  [comboBox.view setFrame:CGRectMake(50, 50, 220, 36)];
+    [self.view addSubview:comboBox];
+    
+    _dataObjects = [NSArray arrayWithObjects:@"House",@"Apple",@"Book",@"Drink", nil];
+    
+    [comboBox setComboBoxDataArray:_dataObjects];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+-(void)comboBox:(ComboBox *)comboBox didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    NSLog(@"%@", [_dataObjects objectAtIndex:indexPath.row]);
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
